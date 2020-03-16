a2936c5b206d0e6fc1d0d5a30033d8f6c90208b1
jaime de Frutos <jaime.defrutos.cerezo@hotmail.com>
2020-03-16 22:30:00 +0100

    intento de paso a pmml

--- Python/ClassModel.py
+++ Python/ClassModel.py
@@ -3,11 +3,8 @@ import pandas as pd
 from ampligraph.utils import restore_model
 from sklearn import metrics
 from xgboost import XGBClassifier
-from ampligraph.evaluation import train_test_split_no_unseen
-from ampligraph.utils import save_model
-from ampligraph.latent_features import ComplEx
-from ampligraph.evaluation import evaluate_performance
-from ampligraph.evaluation import mr_score, mrr_score, hits_at_n_score
+from sklearn2pmml.pipeline import PMMLPipeline
+from sklearn2pmml import sklearn2pmml
 
 
 df = pd.read_pickle("../Data/reducedDataset01.pkl")
@@ -23,7 +20,7 @@ data_embeddings = dict(zip(data, model.get_embeddings(data)))
 
 def get_features_target(mask):
     def get_embeddings(dato):
-        return data_embeddings.get(dato, np.full(200, np.nan))
+        return data_embeddings.get(dato, np.full(400, np.nan))
 
     X = np.vstack(df[mask].data_id.apply(get_embeddings).values)
     y = df[mask].label.values
@@ -33,8 +30,12 @@ clf_X_train, y_train = get_features_target((df["train"]))
 clf_X_test, y_test = get_features_target((~df["train"]))
 
 clf_model = XGBClassifier(n_estimators=500, max_depth=5, objective="multi:softmax")
-clf_model.fit(clf_X_train, y_train)
+pipeline = PMMLPipeline([("classifier", clf_model)])
+#clf_model.fit(clf_X_train, y_train)
+pipeline.fit(clf_X_train, y_train)
+sklearn2pmml(pipeline, "CalssificationModelPipeline.pmml", with_repr = True)
 
-
-print(metrics.classification_report(y_test, clf_model.predict(clf_X_test)))
-print(metrics.confusion_matrix(y_test, clf_model.predict(clf_X_test)))
+#print(metrics.classification_report(y_test, clf_model.predict(clf_X_test)))
+#print(metrics.confusion_matrix(y_test, clf_model.predict(clf_X_test)))
+print(metrics.classification_report(y_test, pipeline.predict(clf_X_test)))
+print(metrics.confusion_matrix(y_test, pipeline.predict(clf_X_test)))
--- Python/EmbeddingModel.py
+++ Python/EmbeddingModel.py
@@ -1,12 +1,14 @@
 # !/usr/bin/env python
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from ampligraph.evaluation import train_test_split_no_unseen
 import tensorflow as tf
 from ampligraph.utils import save_model
 from ampligraph.latent_features import ComplEx
 from ampligraph.evaluation import evaluate_performance
 from ampligraph.evaluation import mr_score, mrr_score, hits_at_n_score
+from sklearn2pmml.pipeline import PMMLPipeline
+from sklearn2pmml import sklearn2pmml
 
 df = pd.read_pickle("../Data/reducedDataset01.pkl")
 
@@ -20,38 +22,37 @@ triples = []
 for _, row in df[df["train"]].iterrows():
     # Data info
     print(row)
-    ecg_triple = ("ECG"+str(row["ECG"]), "isECGDataIn", row["data_id"])
-    emg_triple = ("EMG"+str(row["EMG"]), "isEMGDataIn", row["data_id"])
-    eda_triple = ("EDA"+str(row["EDA"]), "isEDADataIn", row["data_id"])
-    temp_triple = ("TEMP"+str(row["TEMP"]), "isTEMPDataIn", row["data_id"])
-    resp_triple = ("RESP"+str(row["RESP"]), "isRESPDataIn", row["data_id"])
+    ecg_triple = (row["ECG"], "isECGDataIn", row["data_id"])
+    emg_triple = (row["EMG"], "isEMGDataIn", row["data_id"])
+    eda_triple = (row["EDA"], "isEDADataIn", row["data_id"])
+    temp_triple = (row["TEMP"], "isTEMPDataIn", row["data_id"])
+    resp_triple = (row["RESP"], "isRESPDataIn", row["data_id"])
 
     # Stress results
-    if row["label"] == 1:
-        label_triple = (row["data_id"], "ShowsAStateOf", "Baseline")
-    elif row["label"] == 2:
-        label_triple = (row["data_id"], "FeelingOfData", "Stress")
-    elif row["label"] == 3:
-        label_triple = (row["data_id"], "FeelingOfData", "Amusement")
-    elif row["label"] == 4:
-        label_triple = (row["data_id"], "FeelingOfData", "Meditaion")
+    #if row["label"] == 1:
+    #    label_triple = (row["data_id"], "ShowsAStateOf", "Baseline")
+    #elif row["label"] == 2:
+    #    label_triple = (row["data_id"], "ShowsAStateOf", "Stress")
+    #elif row["label"] == 3:
+    #    label_triple = (row["data_id"], "ShowsAStateOf", "Amusement")
+    #elif row["label"] == 4:
+    #    label_triple = (row["data_id"], "ShowsAStateOf", "Meditaion")
 
     suj_triple = (row["data_id"], "isProducedBy", row["subject_id"])
 
     triples.extend((ecg_triple, emg_triple, eda_triple, temp_triple,
-                    resp_triple, label_triple, suj_triple))
+                    resp_triple, suj_triple))
 
 triples_df = pd.DataFrame(triples, columns=["subject", "predicate", "object"])
 triples_df.to_pickle("../Data/tripletsDataset01.pkl")
 
-X_train, X_valid = train_test_split(np.array(triples), test_size=10000, random_state=42)
-print('AAAAAAAAAAAAAAAAAAAAA')
+X_train, X_valid = train_test_split_no_unseen(np.array(triples), test_size=10000, seed=42)
 
 
 
 model = ComplEx(batches_count=50,
                 epochs=200,
-                k=100,
+                k=200,
                 eta=20,
                 optimizer='sgd',
                 optimizer_params={'lr': 1e-4},
@@ -61,19 +62,16 @@ model = ComplEx(batches_count=50,
                 seed=0,
                 verbose=True)
 
-
+pipeline = PMMLPipeline([("classifier", model)])
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-model.fit(X_train)
-
-save_model(model, model_name_path="../Data/KGEmbedModel.pkl")
+#model.fit(X_train)
+pipeline.fit(X_train)
+#save_model(model, model_name_path="../Data/KGEmbedModel.pkl")
+sklearn2pmml(pipeline, "KGEmbedModelPipeline.pmml", with_repr = True)
 
 filter_triples = np.concatenate((X_train, X_valid))
-ranks = evaluate_performance(X_valid,
-                             model=model,
-                             filter_triples=filter_triples,
-                             use_default_protocol=True,
-                             verbose=True,
-			     strict=False)
+#ranks = evaluate_performance(X_valid, model=model, filter_triples=filter_triples, use_default_protocol=True, verbose=True, strict=False)
+ranks = evaluate_performance(X_valid, model=pipeline, filter_triples=filter_triples, use_default_protocol=True, verbose=True, strict=False)
 mr = mr_score(ranks)
 mrr = mrr_score(ranks)
 
@@ -86,4 +84,8 @@ hits_3 = hits_at_n_score(ranks, n=3)
 print("Hits@3: %.2f" % (hits_3))
 hits_1 = hits_at_n_score(ranks, n=1)
 print("Hits@1: %.2f" % (hits_1))
+hits_100 = hits_at_n_score(ranks, n=100)
+print("Hits@100: %.2f" % (hits_100))
+hits_1000 = hits_at_n_score(ranks, n=1000)
+print("Hits@1000: %.2f" % (hits_1000))
 

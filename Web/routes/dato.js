b53387caafdc5eeec4fdc89171264b6bc6a36a4f
jaime de Frutos <jaime.defrutos.cerezo@hotmail.com>
2020-05-01 18:46:15 +0200

    docker-compose file

--- Web/routes/dato.js
+++ Web/routes/dato.js
@@ -48,17 +48,19 @@ router.get('/dato/:id', async(req, res) => {
     const session = driver.session({ defaultAccessMode: neo4j.session.READ });
     session.run('MATCH (n :Subject {val: $value})--(x) RETURN x AS data',{ value: _id })
     .then(result =>{
-      session.close();
+      console.log(result);
       let myData = {}
       result.records.forEach(record => {
         myData[record.get('data').labels[0]] = record.get('data').properties.val
       })
       myData["Subject"] = _id;
       res.status(200).json({'msg': 'TODO OK', 'dato': myData});
+      session.close();
       
     })
     .catch (err =>{
-      console.log(err);
+      console.log("ERRRRROR",err);
+      session.close();
       return res.status(400).json({
         mensaje: 'Ocurrio un error',
         err

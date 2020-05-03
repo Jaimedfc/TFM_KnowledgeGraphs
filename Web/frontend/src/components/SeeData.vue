925a2c148827021b9794f5dd85d105ee7047fbba
jaime de Frutos <jaime.defrutos.cerezo@hotmail.com>
2020-05-03 19:48:15 +0200

    Mejorar Front v3

--- Web/frontend/src/components/SeeData.vue
+++ Web/frontend/src/components/SeeData.vue
@@ -3,7 +3,7 @@
     <b-container>
         <div v-if="cond">
             <h2>Información de {{dato.Sujeto}}</h2>
-            <b-table striped hover :items="dato"></b-table>
+            <b-table striped hover :items="[this.dato]"></b-table>
         </div>
         <div v-else>
             <h2>No se ha especificado ningún dato válido</h2>
@@ -42,20 +42,20 @@ export default {
               .join(". ");
             this.error = error;
           } else {
-            this.dato.Sujeto = result.dato.Subject;
-            this.dato.datoECG = result.dato.ECGData;
-            this.dato.datoEMG = result.dato.EMGData;
-            this.dato.datoEDA = result.dato.EDAData;
-            this.dato.datoTEMP = result.dato.TEMPData;
-            this.dato.datoRESP = result.dato.RESPData;
-            this.dato.Estado = result.dato.Status;
+            this.dato.Sujeto = String(result.dato.Subject);
+            this.dato.datoECG = String(result.dato.ECGData);
+            this.dato.datoEMG = String(result.dato.EMGData);
+            this.dato.datoEDA = String(result.dato.EDAData);
+            this.dato.datoTEMP = String(result.dato.TEMPData);
+            this.dato.datoRESP = String(result.dato.RESPData);
+            this.dato.Estado = String(result.dato.Status);
           }
         });
   },
   computed:{
     cond: function () {
-      return this.dato.Sujeto.length >0 && this.dato.DatoECG.length >0 && this.dato.DatoEMG.length >0
-       && this.dato.DatoEDA.length >0 && this.dato.DatoTEMP.length >0 && this.dato.DatoRESP.length >0 && this.dato.Estado.length >0;
+      return this.dato.Sujeto.length >0 && this.dato.datoECG.length >0 && this.dato.datoEMG.length >0
+       && this.dato.datoEDA.length >0 && this.dato.datoTEMP.length >0 && this.dato.datoRESP.length >0 && this.dato.Estado.length >0;
     }
   }
 };
